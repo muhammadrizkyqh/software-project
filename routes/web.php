5b9ffd7dbb5f900df113337f5542bf3f7125591f
muhammadrizkyqh <muhammadrizkyqh@student.ittelkom-sby.ac.id>
2025-01-10 15:33:30 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookingController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [FilmController::class, 'index'])->name('films.index');
+
+Route::resource('films', FilmController::class);
+
+Route::get('/tes', function(){
+    return view('app.booking.seat-booking');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('films/{film}/book', [BookingController::class, 'bookSeat'])->name('films.book');
+    Route::post('films/{film}/book/checkout', [BookingController::class, 'bookCheckout'])->name('films.book.checkout');
+    Route::post('films/{film}/book/confirm', [BookingController::class, 'confirmCheckout'])->name('films.book.confirm');
+    Route::post('films/{transId}/cancel-transaction', [BookingController::class, 'cancelBooking'])->name('films.transaction.cancel');
+
+    Route::get('user/profile', [UserController::class, 'index'])->name('user.profile');
+    Route::get('user/history-transaction', [UserController::class, 'historyTransaction'])->name('user.history-transaction');
+    Route::get('user/{id}/detail-transaction', [UserController::class, 'detailTransaction'])->name('user.detail-transaction');
+
+
+    Route::post('user/top-up', [UserController::class, 'topupBalance'])->name('user.topup');
+});
+
